7e49bf6c042fe8125d5556b4fcdd92eba59b963d
alekseyen <podkidyshev.as@phystech.edu>
2021-11-07 21:22:23 +0300

    sql_alchemy added. filling with any csv

--- bd_filling/main.py
+++ bd_filling/main.py
@@ -1,22 +1,20 @@
-import psycopg2
+from sqlalchemy import create_engine
+import os
 import pandas as pd
 
-conn = psycopg2.connect(dbname='example',
-                        user='admin',
-                        password='admin',
-                        host='postgres')  # host = NAME of DOCKER Container wiht POSTGRE_SQL
-cursor = conn.cursor()
+# conn = psycopg2.connect(dbname='example',
+#                         user='admin',
+#                         password='admin',
+#                         host='postgres')  # host = NAME of DOCKER Container wiht POSTGRE_SQL
+# cursor = conn.cursor()
 
 # example how to read
 # cursor.execute('SELECT * FROM cats LIMIT 10')
 # records = cursor.fetchall()
 
+
+engine = create_engine(os.getenv('DATABASE_CONNECTION_URL'), echo=True)
 data_to_record = pd.read_csv('../data/cats.csv', delimiter=';')
 
-with open('data/cats.csv', 'r') as f:
-    cursor.copy_from(f, 'cats', sep=';')
+data_to_record.to_sql('cats', con=engine, if_exists='fail')
 
-conn.commit()
-
-cursor.close()
-conn.close()
--- databases/create_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS cats (
-    name text,
-    price int,
-    breed text
---   PRIMARY KEY (product_id)
-);
