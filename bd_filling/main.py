28fd20782d70b17f15af9a4fb97915b152c4ec18
alekseyen <podkidyshev.as@phystech.edu>
2021-11-07 23:59:56 +0300

    fix

--- bd_filling/main.py
+++ bd_filling/main.py
@@ -12,9 +12,7 @@ import pandas as pd
 # cursor.execute('SELECT * FROM cats LIMIT 10')
 # records = cursor.fetchall()
 
-
 engine = create_engine(os.getenv('DATABASE_CONNECTION_URL'), echo=True)
 data_to_record = pd.read_csv('../data/cats.csv', delimiter=';')
 
-data_to_record.to_sql('cats', con=engine, if_exists='fail')
-
+data_to_record.to_sql('cats', con=engine, if_exists='fail', index=False)
